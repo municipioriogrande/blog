591bc666c5071f4d3c5c24a105b73611efc5891d
aquinzi <arielaquinzi@gmail.com>
2019-07-26 10:49:44 -0300

    agregado actualización estilo y template para la migración hecha a gutenberg

---
+++ themes/rgblog/tpl-hubspot.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Page style Hubspot
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package rgblog
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			//get_template_part( 'template-parts/content', 'page' );
+			?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="entry-content">
+					<?php
+					//	the_title( '<h2>', '</h2>' );
+					the_content();
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'rgblog' ),
+						'after'  => '</div>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+
+	
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+			<?php
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
